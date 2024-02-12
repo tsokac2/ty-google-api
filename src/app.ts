b2ce78d8db7352aa07d1268db05f20bfa81c52ff
unknown <tsokac2@gmail.com>
2024-02-12 15:46:56 +0000

    Updating

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,12 @@
-const from = document.querySelector('form');
+const from = document.querySelector('form')!;
+const addressInput = document.getElementById('address')! as HTMLInputElement;
+
+function searchAddressHandler(event: Event){
+    event.preventDefault();
+    const enteredAddress = addressInput.ariaValueMax;
+
+    // send this to Google's API!
+}
+
+from?.addEventListener('submit',);
 
