384597abce51c1ab989e1bbcb7ed1f611e762914
unknown <tsokac2@gmail.com>
2024-02-12 16:46:41 +0000

    Updating

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,19 @@
 const from = document.querySelector('form')!;
 const addressInput = document.getElementById('address')! as HTMLInputElement;
 
+const GOOGLE_API_KEY = 'AIzaSyD1ov2el6bz3gUI8qebqbndaTE9itkTnV4';
+
 function searchAddressHandler(event: Event){
     event.preventDefault();
     const enteredAddress = addressInput.ariaValueMax;
 
-    // send this to Google's API!
+ 
 }
 
 from?.addEventListener('submit', searchAddressHandler);
 
+
+
+
+// AIzaSyD1ov2el6bz3gUI8qebqbndaTE9itkTnV4
+//https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=
